53dd52bd990753a1c225e45adce33ae8f65b72f6
sign <sign1113@ymail.com>
2023-08-04 00:15:51 +0800

    8/4

--- routes/forum.js
+++ routes/forum.js
@@ -25,29 +25,17 @@ router.get("/", async (req, res) => {
 
       // 查詢所有留言資料及論壇資料
       const sql = `
-        SELECT 
-          m.message_sid,
-          m.publishedTime,
-          m.user_id,
-          m.forum_sid,
-          m.comment_content,
-          f.header,
-          f.content as forum_content,
-          f.photo as forum_photo,
-          mi.sid as user_id,
-          mi.nickname,
-          mi.photo as user_photo,
-          c.comment_count
-        FROM message m
-        JOIN forum f ON m.forum_sid = f.forum_sid
-        JOIN member_info mi ON m.user_id = mi.sid
-        JOIN (
-            SELECT forum_sid, COUNT(comment_content) AS comment_count
-            FROM message
-            GROUP BY forum_sid
-        ) c ON m.forum_sid = c.forum_sid
-        WHERE f.header LIKE ${kw_escaped}
-        OR f.content LIKE ${kw_escaped}
+      SELECT 
+      f.forum_sid,
+      f.header,
+      f.content AS forum_content,
+      f.photo AS forum_photo,
+      mi.sid AS member_sid,
+      mi.nickname,
+      mi.photo AS user_photo
+  FROM forum f
+  JOIN member_info mi ON f.user_id = mi.sid;
+  
       `;
       const [messages] = await db.query(sql);
 
@@ -83,18 +71,30 @@ router.post("/add", multipartParser, async (req, res) => {
     return res.json(output);
   }
 });
+
 router.get("/forum/:forum_sid", async (req, res) => {
   const { forum_sid } = req.params;
 
   // 查詢論壇資料
-  const forum_sql = `SELECT * FROM forum WHERE forum_sid = ${forum_sid} ORDER BY publishedTime DESC`; 
+  const forum_sql = `
+  SELECT 
+  f.forum_sid,
+  f.header,
+  f.content AS forum_content,
+  f.photo AS forum_photo,
+  mi.sid AS member_sid,
+  mi.nickname,
+  mi.photo AS user_photo
+FROM forum f
+JOIN member_info mi ON f.user_id = mi.sid
+WHERE f.forum_sid = ${forum_sid}`;
   const [forum_data] = await db.query(forum_sql);
 
   forum_data.forEach((i) => {
     i.publishedTime = dayjs(i.publishedTime).format("YYYY-MM-DD HH:mm:ss");
   });
 
-  // 查詢對應的留言資料
+  // 查詢對應的留言資料 ----****
   const message_sql = `SELECT 
     m.message_sid,
     m.publishedTime,
@@ -102,7 +102,8 @@ router.get("/forum/:forum_sid", async (req, res) => {
     m.forum_sid,
     m.comment_content,
     mi.sid AS member_sid,
-    mi.nickname
+    mi.nickname,
+    mi.photo
   FROM 
     message m
   JOIN 
@@ -110,57 +111,90 @@ router.get("/forum/:forum_sid", async (req, res) => {
   WHERE m.forum_sid = ${forum_sid}`;
   const [messageData] = await db.query(message_sql);
 
-  
   // 合併論壇資料和留言資料到 output 物件
   const output = {
     forum_data: forum_data,
-    messageData: messageData
+    messageData: messageData,
   };
 
   return res.json(output);
 });
-router.get("/message", async (req, res) => {
- 
-  const sql = `
-    SELECT 
-        m.message_sid,
-        m.publishedTime,
-        m.user_id,
-        m.forum_sid,
-        m.comment_content,
-        f.header,
-        f.content as forum_content,
-        f.photo as forum_photo,
-        mi.sid as user_id,
-        mi.nickname,
-        mi.photo as user_photo,
-        c.comment_count
-    FROM message m
-    JOIN forum f ON m.forum_sid = f.forum_sid
-    JOIN member_info mi ON m.user_id = mi.sid
-    JOIN (
-        SELECT forum_sid, COUNT(comment_content) AS comment_count
-        FROM message
-        GROUP BY forum_sid
-    ) c ON m.forum_sid = c.forum_sid
-    WHERE 1;
-  `;
-  
-  const [messages] = await db.query(sql);
 
-  
-  const keyword = req.query.forum_keyword;
-  let searchResult = [];
+router.get("/message", async (req, res) => {
+  console.log(req.query.forum_keyword);
+
+  let where = " where 1 ";
+  let order = "";
+
+  const keyword = req.query.forum_keyword || "";
+  const orderBy = req.query.forum_orderBy || "";
+  console.log(orderBy);
+  const page = parseInt(req.query.forum_page) || 1;
+  const limit = 100;
+  const offset = (page - 1) * limit;
+
   if (keyword) {
     const kw_escaped = db.escape("%" + keyword + "%");
-    const searchSql = `
-      SELECT * FROM forum
-      WHERE header LIKE ${kw_escaped}
-      OR content LIKE ${kw_escaped}
-    `;
-    [searchResult] = await db.query(searchSql);
-  } 
+    where += `AND f.header LIKE ${kw_escaped} OR f.content LIKE ${kw_escaped}`;
+  }
 
-  res.json({ messages, searchResult });
+  if (orderBy) {
+    order = `ORDER BY f.publishedTime ${orderBy}`;
+  }
+  const sql = `
+  SELECT 
+  f.forum_sid,
+  f.header,
+  f.publishedTime,
+  f.content AS forum_content,
+  f.photo AS forum_photo,
+  mi.sid AS member_sid,
+  mi.nickname,
+  mi.photo AS user_photo,
+  COALESCE(c.comment_count, 0) AS comment_count,
+  COALESCE(l.like_count, 0) AS like_count
+FROM forum f
+JOIN member_info mi ON f.user_id = mi.sid
+LEFT JOIN (
+    SELECT forum_sid, COUNT(comment_content) AS comment_count
+    FROM message
+    GROUP BY forum_sid
+) c ON f.forum_sid = c.forum_sid
+LEFT JOIN (
+    SELECT forum_sid, COUNT(like_sid) AS like_count
+    FROM forum_like
+    GROUP BY forum_sid
+) l ON f.forum_sid = l.forum_sid
+${where}
+${order}
+LIMIT ${offset}, ${limit}
+`;
+
+  const [article] = await db.query(sql);
+
+  // let searchResult = [];
+
+  res.json(article);
+  console.log(article);
+});
+// 新增留言的api
+router.post("/addmessage", multipartParser, async (req, res) => {
+  let output = {
+    success: true,
+  };
+
+  const {  user_id , comment_content } = req.body;
+  // 將留言資料插入到資料庫中
+  const sql = `
+    INSERT INTO message ( user_id ,comment_content , publishedTime)
+    VALUES (?,?,NOW())`;
+  try {
+    await db.query(sql, [ user_id , comment_content ]);
+    return res.json(output);
+  } catch (error) {
+    console.error(error);
+    output.success = false;
+    return res.json(output);
+  }
 });
 module.exports = router;
